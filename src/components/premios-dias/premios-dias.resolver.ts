3889d334196447d0722b434cd1b5eb3204a899db
Carlos Diaz <c.diazadriann@gmail.com>
2023-03-01 12:36:01 -0400

    Me falta el CRUD de Response

--- src/components/premios-dias/premios-dias.resolver.ts
+++ src/components/premios-dias/premios-dias.resolver.ts
@@ -1,35 +1,78 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { ParseIntPipe, UseGuards } from '@nestjs/common';
+
+//PROPIO
 import { PremiosDiasService } from './premios-dias.service';
 import { PremiosDia } from './entities/premios-dia.entity';
 import { CreatePremiosDiaInput } from './dto/create-premios-dia.input';
 import { UpdatePremiosDiaInput } from './dto/update-premios-dia.input';
+import { ResponsePropioGQl } from './../../common/response';
+import { JwtAuthGuard } from './../../auth/guards/jwt-auth.guard';
+import { VALID_PERMISO_ACCION } from './../../config/valid-roles';
+import { PaginationArgs } from './../../common/dto/args';
+import { CurrentUser } from './../../auth/decorators/current-user.decorator';
+import { User } from '../users/entities/user.entity';
 
+@UseGuards(JwtAuthGuard)
 @Resolver(() => PremiosDia)
 export class PremiosDiasResolver {
   constructor(private readonly premiosDiasService: PremiosDiasService) {}
 
-  @Mutation(() => PremiosDia)
-  createPremiosDia(@Args('createPremiosDiaInput') createPremiosDiaInput: CreatePremiosDiaInput) {
+  @Mutation(() => PremiosDia, {
+    name: 'createPremisoDias',
+    description: 'Para crear el dia de un Premio Automatico de Lotenet',
+  })
+  async createPremiosDia(
+    @CurrentUser([VALID_PERMISO_ACCION.LOTENET_PREMIO_CREATE]) user: User,
+    @Args('createPremiosDiaInput') createPremiosDiaInput: CreatePremiosDiaInput,
+  ): Promise<PremiosDia> {
     return this.premiosDiasService.create(createPremiosDiaInput);
   }
 
-  @Query(() => [PremiosDia], { name: 'premiosDias' })
-  findAll() {
-    return this.premiosDiasService.findAll();
+  @Query(() => [PremiosDia], {
+    name: 'allPremiosDias',
+    description: 'Ver todos los dias de los premios',
+  })
+  async findAll(
+    @CurrentUser([VALID_PERMISO_ACCION.LOTENET_PREMIO_VIEW]) user: User,
+    @Args() paginationArgs: PaginationArgs,
+  ): Promise<PremiosDia[]> {
+    return this.premiosDiasService.findAll(paginationArgs);
   }
 
-  @Query(() => PremiosDia, { name: 'premiosDia' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => PremiosDia, {
+    name: 'findPremiosDia',
+    description: 'Ver un dia especifico de premio',
+  })
+  async findOne(
+    @CurrentUser([VALID_PERMISO_ACCION.LOTENET_PREMIO_VIEW]) user: User,
+    @Args('id', { type: () => Int }, ParseIntPipe) id: number,
+  ): Promise<PremiosDia> {
     return this.premiosDiasService.findOne(id);
   }
 
-  @Mutation(() => PremiosDia)
-  updatePremiosDia(@Args('updatePremiosDiaInput') updatePremiosDiaInput: UpdatePremiosDiaInput) {
-    return this.premiosDiasService.update(updatePremiosDiaInput.id, updatePremiosDiaInput);
+  @Mutation(() => PremiosDia, {
+    name: 'updatePremiosDias',
+    description: 'Actualizar el dia de un premio',
+  })
+  async updatePremiosDia(
+    @CurrentUser([VALID_PERMISO_ACCION.LOTENET_PREMIO_UPDATE]) user: User,
+    @Args('updatePremiosDiaInput') updatePremiosDiaInput: UpdatePremiosDiaInput,
+  ): Promise<PremiosDia> {
+    return this.premiosDiasService.update(
+      updatePremiosDiaInput.id,
+      updatePremiosDiaInput,
+    );
   }
 
-  @Mutation(() => PremiosDia)
-  removePremiosDia(@Args('id', { type: () => Int }) id: number) {
+  @Mutation(() => ResponsePropioGQl, {
+    name: 'removePremioDias',
+    description: 'Eliminar el dia de un Premio',
+  })
+  async removePremiosDia(
+    @CurrentUser([VALID_PERMISO_ACCION.LOTENET_PREMIO_DELETE]) user: User,
+    @Args('id', { type: () => Int }, ParseIntPipe) id: number,
+  ): Promise<ResponsePropioGQl> {
     return this.premiosDiasService.remove(id);
   }
 }
--- src/components/premios-dias/premios-dias.service.ts
+++ src/components/premios-dias/premios-dias.service.ts
@@ -1,26 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreatePremiosDiaInput } from './dto/create-premios-dia.input';
 import { UpdatePremiosDiaInput } from './dto/update-premios-dia.input';
+import { PremiosDia } from './entities/premios-dia.entity';
+import { PaginationArgs } from 'src/common/dto/args';
+import { ResponsePropioGQl } from 'src/common/response';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MESSAGE } from 'src/config/messages';
 
 @Injectable()
 export class PremiosDiasService {
-  create(createPremiosDiaInput: CreatePremiosDiaInput) {
-    return 'This action adds a new premiosDia';
+  constructor(
+    @InjectRepository(PremiosDia)
+    private readonly premiosDiasRepository: Repository<PremiosDia>,
+  ) {}
+
+  async create(
+    createPremiosDiaInput: CreatePremiosDiaInput,
+  ): Promise<PremiosDia> {
+    try {
+      const { id_dia, id_lotenet_premio, ...rest } = createPremiosDiaInput;
+
+      const newSorteo = this.premiosDiasRepository.create({
+        ...rest,
+        dias: { id: id_dia },
+        lotenet_premio: { id: id_lotenet_premio },
+      });
+
+      await this.premiosDiasRepository.save(newSorteo);
+      return await this.findOne(newSorteo.id);
+    } catch (error) {
+      throw new UnprocessableEntityException(error?.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all premiosDias`;
+  async findAll(paginationArgs: PaginationArgs): Promise<PremiosDia[]> {
+    const { limit, offset } = paginationArgs;
+    return await this.premiosDiasRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} premiosDia`;
+  async findOne(id: number): Promise<PremiosDia> {
+    const premiosDias = await this.premiosDiasRepository.findOneBy({ id });
+    if (!premiosDias) {
+      throw new NotFoundException(MESSAGE.COMUN_ESTE_ID_NO_EXISTE);
+    }
+    return premiosDias;
   }
 
-  update(id: number, updatePremiosDiaInput: UpdatePremiosDiaInput) {
-    return `This action updates a #${id} premiosDia`;
+  async update(
+    id: number,
+    updatePremiosDiaInput: UpdatePremiosDiaInput,
+  ): Promise<PremiosDia> {
+    throw new BadGatewayException(MESSAGE.FALTA_IMPLEMENTAR_ESTE_METODO);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} premiosDia`;
+  async remove(id: number): Promise<ResponsePropioGQl> {
+    const premio_dia = await this.findOne(id);
+    try {
+      await this.premiosDiasRepository.remove(premio_dia);
+      return {
+        message: MESSAGE.COMUN_SE_ELIMINO_CORRECTAMENTE,
+        status: 200,
+        error: false,
+      };
+    } catch (error) {
+      return {
+        message: MESSAGE.COMUN_NO_SE_PUDO_ELIMINAR,
+        status: 401,
+        error: true,
+      };
+    }
   }
 }
