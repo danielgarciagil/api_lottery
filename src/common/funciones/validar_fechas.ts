2173d499a1a2173d5a21ff2ca42013f5027243ba
Carlos Diaz <c.diazadriann@gmail.com>
2023-03-05 22:36:08 -0400

    dsds

--- src/common/funciones/validar_fechas.ts
+++ src/common/funciones/validar_fechas.ts
@@ -5,33 +5,31 @@ export const arrFechasHoy = (): string[] => {
   const fechaDiaUnDigito = fecha.format('D').replace(/^0+/, '');
 
   return [
-    fecha.format('YYYY-MM-DD'), // PRIMERA
-    fecha.format('DD-MM-YYYY'), // LOTERIAS DOMINICANAS / REAL
+    fecha.format('YYYY-MM-DD').toUpperCase(), // PRIMERA
+    fecha.format('DD-MM-YYYY').toUpperCase(), // LOTERIAS DOMINICANAS / REAL
     fecha.locale('es').format('dddd, DD-MM-YYYY').toUpperCase(), // LOTEDOM
-    fecha.locale('en').format('dddd, MMMM D, YYYY'), //FLORIDA OFICIAL
-    fecha.format('DD/MM/YYYY'), //KING OFFICIAL
+    fecha.locale('en').format('dddd, MMMM D, YYYY').toUpperCase(), //FLORIDA OFICIAL
+    fecha.format('DD/MM/YYYY'), //KING OFFICIAL //LOTEKA
+    fecha
+      .locale('es')
+      .format('[Sorteo]: DD [de] MMMM [del] YYYY[.]')
+      .toUpperCase(), //LEISA
+    fecha.locale('en').format('ddd MM/DD/YY').toUpperCase(), //NEW YORK OFICIAL
     //!Falta por probar
-    fecha.format(`dddd, MMMM ${fechaDiaUnDigito}, YYYY`),
-    fecha.format('dddd, MMM D, YYYY'),
-    fecha.format(`dddd, MMM ${fechaDiaUnDigito}, YYYY`),
-    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[th] YYYY`),
-    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[st] YYYY`),
-    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[nd] YYYY`),
-    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[rd] YYYY`),
-    fecha.format('ddd MM/DD/YYYY'),
-    fecha.format('dddd MMM Dth YYYY'),
-    fecha.format('ddd MM/DD/YY'),
-    fecha.format('dddd, MMM D, YYYY'),
-    fecha.format('[Resultados] MM/DD/YYYY'),
+
+    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[th] YYYY`).toUpperCase(),
+    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[st] YYYY`).toUpperCase(),
+    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[nd] YYYY`).toUpperCase(),
+    fecha.format(`dddd MMMM ${fechaDiaUnDigito}[rd] YYYY`).toUpperCase(),
   ];
 };
-
 export const validarFechaQueSeaDeHoy = (
   fechaComprobarXpath: string,
   arrFecha: string[],
 ): string => {
-  console.log(fechaComprobarXpath);
-  const is_fecha = arrFecha.includes(fechaComprobarXpath);
+  console.log(`|${fechaComprobarXpath.trim().toUpperCase()}|`);
+  console.log(arrFecha);
+  const is_fecha = arrFecha.includes(fechaComprobarXpath.trim().toUpperCase());
   if (!is_fecha) throw new Error('NO ES LA FECHA A BUSCAR');
   return fechaComprobarXpath;
 };
